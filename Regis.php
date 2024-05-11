287de712c5d694f0fe1d9cc5c4c397b19ec67d8b
TripPlanner01 <tripplannerr1@gmail.com>
2024-05-11 13:42:47 +0530

    Add files via upload
    Uploaded all files

---
+++ Regis.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <form action="Reg.php" method=POST>
+            UserName <input type="text" name='name'><br>
+            Enrollment <input type="text" name='enroll'><br>
+            Email <input type="text" name='email'><br>
+            Password <input type="text" name='pass'><br>
+            <input type="Submit">
+        </form>
+    </body>
+</html>
